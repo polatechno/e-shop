c12422fea2c2f1ef9dc7690a1d2bc067a4d1049c
polatechno <aganiyazpolat@gmail.com>
2024-10-03 18:48:35 +0500

    Put supabase url in env file

--- supabase/client.ts
+++ supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export const createClient = () =>
     createBrowserClient(
-        "https://imjlqgjlwweruvhvhyid.supabase.co",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltamxxZ2psd3dlcnV2aHZoeWlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MDU0OTIsImV4cCI6MjA0MzI4MTQ5Mn0.STuy4L6lQLK4h-nMx2U-iRRRTVTphyNHyLd2yJmdm8M",
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 
     )
